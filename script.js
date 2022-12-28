e6094dac7a39d63804f873a108a13ecc96a75747
Bruno oliveira <brunogabriel2608@gmail.com>
2022-12-27 23:23:38 -0300

    jogo pronto

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 const kakashi = document.querySelector(".character");
 const trunk = document.querySelector(".trunk");
+const points = document.querySelector(".points");
+
+let point = 0;
+
+
 const jump = () =>
 {
-    
+    var trunkl = trunk.offsetLeft;
+    kakashi.src = 'assets/jump.png';
     kakashi.classList.add('jump');
     setTimeout(() => {
+        kakashi.src = 'assets/kakashi.gif';
         kakashi.classList.remove('jump');
     }, "500");
-    
+    if(trunkl > -80 && trunkl < 173)
+    {
+        point++;
+        
+    }
+    points.textContent = `Score = ${point}`;
 }
+
 
 
 const loop = setInterval(() => {
     
+
     const kakashibottom = +window.getComputedStyle(kakashi).bottom.replace('px', '');
     const trunkleft = trunk.offsetLeft;
-    console.log(trunkleft)
 
 
-    if(trunkleft <= -95 && trunkleft > -163 && kakashibottom < 60)
+    if(trunkleft <= -90 && trunkleft > -163 && kakashibottom < 60)
     {
+        kakashi.src = 'assets/jump.png';
+        
+        document.removeEventListener('keydown', jump);
+        
         trunk.style.animation = 'none';
         trunk.style.left = `${trunkleft}px`;
-
+        
+        
         kakashi.classList.remove('jump');
         kakashi.style.bottom = `${kakashibottom}px`
 
-        
+        clearInterval(loop);
+
     }
 }, 10);
+
+
 
 document.addEventListener('keydown', jump);
 
